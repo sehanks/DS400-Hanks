4c44d7ec1ac5c6b1cd9ccee63e0358d3ee78ef2d
sehanks <116026529+sehanks@users.noreply.github.com>
2023-03-29 12:06:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,48 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Load model
+model = load_model('model_cnn.hdf5')
 
+# Datetime
+starttime = datetime.now()
+
+# Function to save the audio file
+def save_audio_file(file):
+    
+    if file.size > 5000000:
+        return 1
+    
+    folder = 'audio'
+    
+    # Convert current date and time to a string
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    
+    # Clear folder
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            # Check whether the specified path is an existing file or not
+            # Check whether the given path represents an existing directory entry that is a symbolic link or not
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                # Deletes the file path
+                os.unlink(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    try:
+        with open('log0.txt', 'a') as f:
+            f.write(f'{file.name} - {file.size} - {date};\n')
+    except:
+        pass
+
+    with open(os.path.join(folder, file.name), 'wb') as f:
+        # Returns a generated PDF document as a byte array
+        f.write(file.getbuffer())
+        
+    return 0
+
+# Creating the app
 def main():
     
     # Image
@@ -33,6 +74,15 @@ def main():
             with col1:
                 # File uploader
                 audio = st.file_uploader('Upload your audio file', type = ['wav', 'mp3'])
+                if audio is not None:
+                    # Check whether the specified path exists or not
+                    if not os.path.exists('audio'):
+                        # Create a directory recursively
+                        os.makedirs('audio')
+                    # Join different path components
+                    path = os.path.join('audio', audio.name)
+                    # save_audio_file function
+                    save_audio_file = save_audio_file(audio)
                   
         
         
