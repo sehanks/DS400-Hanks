425c63a4e3eda5cf2dd13d0234064a30f8f9000d
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-03 19:36:12 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -123,16 +123,21 @@ def main():
                 else:
                     column3, column4 = st.columns(2)
                     with column3:  
-                        if st.button('Try test audio file'):
+                        if st.button('Try test audio file'):  # Test audio file button
                             st.audio(data = 'Application/OAF_back_angry.wav', format = 'audio/wav', start_time = 0) 
                             path = 'Application/OAF_back_angry.wav'
                             wav, sr = librosa.load(path, sr = 45000)
                             array, sampling_rate = librosa.load(path)
                             waveplot(array, sampling_rate, 'Angry')
                             spectrogram(array, sampling_rate, 'Angry')
+                            fig = plt.figure(figsize = (10, 2))
+                            plt.title('Waveplot for Test Audio File')
+                            array, sampling_rate = librosa.load(path)
+                            waveplot(array, sampling_rate, 'Angry')
+                            st.write(fig)
                     with column4:
                         if audio_file is None:
-                            if st.button('Record an audio file'):
+                            if st.button('Record an audio file'):  # Record audio button
                                 with st.spinner(f'Recording for 5 seconds ....'):
                                     st.write('Recording...')
                                     time.sleep(3)
