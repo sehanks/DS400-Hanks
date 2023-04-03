decbd3722091cc80ce78a06db14941b5ef17d3a6
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-03 19:28:15 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -124,9 +124,9 @@ def main():
                     column3, column4 = st.columns(2)
                     with column3:  
                         if st.button('Try test audio file'):
-                            wav, sr = librosa.load(path, sr = 45000)
                             st.audio(data = 'Application/OAF_back_angry.wav', format = 'audio/wav', start_time = 0) 
                             path = 'Application/OAF_back_angry.wav'
+                            wav, sr = librosa.load(path, sr = 45000)
                             array, sampling_rate = librosa.load(path)
                             waveplot(array, sampling_rate, 'Angry')
                             spectrogram(array, sampling_rate, 'Angry')
