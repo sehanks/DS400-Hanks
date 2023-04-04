2362d2528f2dbae56da82f93b4d0f92ac3685dd5
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-03 20:58:29 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -148,10 +148,8 @@ def main():
                                 st.markdown('##### Mel-Spectrogram for Test Audio File')
                                 fig2 = plt.figure(figsize = (20, 8))
                                 mel = librosa.feature.melspectrogram(y = wav, sr = 45000)
-                                librosa.display.specshow(mel, x_axis = 'time')
-                                
-                                #S = librosa.feature.melspectrogram(y = wav, sr = 45000, n_mels = 128, fmax = 8000)
-                                #librosa.display.specshow(S, sr = 45000, x_axis = 'time', y_axis = 'hz')
+                                img = librosa.display.specshow(mel, x_axis = 'time', y_axis = 'hz')
+                                fig2.colorbar(img)
                                 st.write(fig2)
                     with column4:
                         if audio_file is None:
