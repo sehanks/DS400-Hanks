bbe6427c0060eb425aae78edc0c266698b037911
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-04 14:20:57 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -162,7 +162,7 @@ def main():
                             with column2:
                                 st.markdown('#  ')
                                 st.markdown('###  ')
-                                st.markdown('####   ')
+                                st.markdown('#####   ')
                                 st.markdown('###### Mel-Spectrogram for Test Audio File')
                                 fig2 = plt.figure(figsize = (20, 8))
                                 spectrogram(wav, sr)
