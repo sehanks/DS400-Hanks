76e9ae1c3149203e0fb2f6e24f9c6f958ab09224
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-11 12:34:21 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -285,7 +285,7 @@ def main():
                 feature = pd.read_csv('Application/feat.csv')
                 
                 # Prediction
-                get_pred('Application/OAF_back_angry.wav')[0]
+                get_pred('Application/OAF_back_angry.wav')[4]
                 
                 
                  
