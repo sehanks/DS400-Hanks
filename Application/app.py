7ebd260c80924814128ec20151243a5146165a44
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-13 19:03:24 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -296,6 +296,20 @@ def main():
                         st.markdown('#### Emotion Detected: {}'.format(pred_emotion[2]))
                 with column6:
                     if audio_file == 'test_file':
+                        st.markdown('#  ')
+                        fig = plt.figure(figsize = (10, 4))
+                        unique, counts = np.unique(pred_emotion ,return_counts = True)
+                        bar = np.asarray((unique, counts)).T
+                        col = list(bar[:, 0])
+                        row = list(bar[:, 1])
+                        color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+                        plt.bar(x = col, height = [int(x) for x in row], color = color)
+                        plt.xlabel('Emotions Detected')
+                        plt.ylabel('Frequency')
+                        plt.title('Frequency of Emotions Detected')
+                        st.pyplot(fig)
+                    if not audio_file == 'test_file':
+                        st.markdown('#  ')
                         fig = plt.figure(figsize = (10, 4))
                         unique, counts = np.unique(pred_emotion ,return_counts = True)
                         bar = np.asarray((unique, counts)).T
