dae63cca758356111a6a1012f5bc9ca51611b9a0
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-13 19:07:03 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -288,12 +288,12 @@ def main():
                     if audio_file == 'test_file':
                         pred_emotion = get_pred('Application/OAF_back_angry.wav')
                         st.markdown('#  ')
-                        st.markdown('#### Emotion Detected: {}'.format(pred_emotion[2]))
+                        st.markdown('##### Emotion Detected: {}'.format(pred_emotion[2]))
                         #st.markdown('#### Emotion Detected: Angry')
                     if not audio_file == 'test_file':
                         pred_emotion = get_pred(audio_file)
                         st.markdown('#  ')
-                        st.markdown('#### Emotion Detected: {}'.format(pred_emotion[2]))
+                        st.markdown('##### Emotion Detected: {}'.format(pred_emotion[2]))
                 with column6:
                     if audio_file == 'test_file':
                         st.markdown('#  ')
