24b1f0f014ae7c09b60dd8740637f69512422dd9
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-14 16:21:59 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -248,9 +248,11 @@ def main():
                                 st.write(fig2)
                     with column4:
                         if st.button('Record an audio file'):
-                            #audio = audiorecorder('Click to record', 'Recording...')
-                            audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=42000)
-                            st.markdown(audio_bytes)
+                            audio = audiorecorder('Click to record', 'Recording...')
+                            #audio_bytes = audio_recorder(pause_threshold=2.0, sample_rate=42000)
+                            #st.markdown(audio)
+                            if len(audio) > 0:
+                                st.audio(audio.tobytes())
                                 
                                 
                                 
