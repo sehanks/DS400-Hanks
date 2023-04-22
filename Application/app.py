9a884e105df79852b2ee92606cd6fb7099fecf4d
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-22 18:08:33 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -375,8 +375,7 @@ def main():
                 if audio_file == 'test_file':
                     #pred_emotion = get_pred('Application/OAF_back_sad.wav')
                     #unique, counts = np.unique(pred_emotion, return_counts = True)
-                    #st.markdown('## Emotion Detected: {}'.format(unique[counts.argmax()]))
-                    total_pred = [] 
+                    st.markdown('## Emotion Detected: {}'.format(np.argmax(predictions))
                     X = feature('Application/OAF_back_sad.wav')
                     predictions = model.predict(X, use_multiprocessing = True)
                     list_predictions = list(predictions)
@@ -385,7 +384,7 @@ def main():
                 if not audio_file == 'test_file':
                     #pred_emotion = get_pred(audio_file)
                     #unique, counts = np.unique(pred_emotion, return_counts = True)
-                    #st.markdown('## Emotion Detected: {}'.format(unique[counts.argmax()]))
+                    st.markdown('## Emotion Detected: {}'.format(np.argmax(predictions))
                     X = feature(audio_file)
                     predictions = model.predict(X, use_multiprocessing = True)
                     list_predictions = list(predictions)
