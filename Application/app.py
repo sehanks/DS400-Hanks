f7737300d11604acd1a915a5647fab916dc34a73
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-23 19:37:04 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -258,7 +258,7 @@ def main():
                                 st.write(fig2)
                     with column4:
                         audio_bytes = audio_recorder(text = 'Click to Record', 
-                                                     recording_color="#2c7de8", neutral_color = '2cd2e8', icon_name = 'volume-high', icon_size="2x")
+                                                     recording_color = '2cd2e8', neutral_color = '2c7de8', icon_name = 'volume-high', icon_size="2x")
                         if audio_bytes:
                             st.audio(audio_bytes, format = 'audio/wav')
                         
