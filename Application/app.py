f1f42392143a46d68ca2658d102bd704fa0687e6
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-23 22:20:35 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -324,30 +324,30 @@ def main():
                                 st.markdown('#  ')
                                 st.audio(audio_bytes, format = 'audio/wav')
                                 
-                        with column1: 
-                                st.markdown('#  ')
-                                st.markdown('###### Waveplot for Recorded Audio File')
-                                fig = plt.figure(figsize = (20, 8))
-                                np_bytes = BytesIO(audio_bytes)
-                                wav, sr = librosa.load(np_bytes, sr = 45000)
-                                librosa.display.waveplot(wav, sr = 45000)
-                                plt.gca().axes.get_yaxis().set_visible(False)
-                                plt.gca().axes.get_xaxis().set_visible(False)
-                                plt.gca().axes.spines['right'].set_visible(False)
-                                plt.gca().axes.spines['left'].set_visible(False)
-                                plt.gca().axes.spines['top'].set_visible(False)
-                                plt.gca().axes.spines['bottom'].set_visible(False)
-                                st.write(fig)
-                        with column2:
-                                st.markdown('#  ')
-                                st.markdown('####  ')
-                                st.markdown('######   ')
-                                st.markdown('###### Mel-Spectrogram for Test Audio File')
-                                fig2 = plt.figure(figsize = (20, 8))
-                                spectrogram(wav, sr)
-                                st.write(fig2)
-                        with st.container():
-                            pred_emotion = get_pred_recorded(np_bytes, sr)
+                                with column1: 
+                                    st.markdown('#  ')
+                                    st.markdown('###### Waveplot for Recorded Audio File')
+                                    fig = plt.figure(figsize = (20, 8))
+                                    np_bytes = BytesIO(audio_bytes)
+                                    wav, sr = librosa.load(np_bytes, sr = 45000)
+                                    librosa.display.waveplot(wav, sr = 45000)
+                                    plt.gca().axes.get_yaxis().set_visible(False)
+                                    plt.gca().axes.get_xaxis().set_visible(False)
+                                    plt.gca().axes.spines['right'].set_visible(False)
+                                    plt.gca().axes.spines['left'].set_visible(False)
+                                    plt.gca().axes.spines['top'].set_visible(False)
+                                    plt.gca().axes.spines['bottom'].set_visible(False)
+                                    st.write(fig)
+                                with column2:
+                                    st.markdown('#  ')
+                                    st.markdown('####  ')
+                                    st.markdown('######   ')
+                                    st.markdown('###### Mel-Spectrogram for Test Audio File')
+                                    fig2 = plt.figure(figsize = (20, 8))
+                                    spectrogram(wav, sr)
+                                    st.write(fig2)
+                                with st.container():
+                                    pred_emotion = get_pred_recorded(np_bytes, sr)
                             
                      
                         
