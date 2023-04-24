c0cd3dca8b2ddb40f15fae72a592fb6c7f239050
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-23 20:32:48 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -259,7 +259,7 @@ def main():
                                 st.markdown('#  ')
                                 st.markdown('#  ')
                                 st.audio(audio_bytes, format = 'audio/wav')
-                                wav = np.frombuffer(audio_bytes, dtype = 'int32')
+                                wav = np.frombuffer(audio_bytes, dtype = 'float32')
                         with column1: 
                                 st.markdown('#  ')
                                 st.markdown('###### Waveplot for Recorded Audio File')
