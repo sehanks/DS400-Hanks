290eaa508990996fcb2c0de4d4deb52a7431f996
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-24 17:07:29 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -341,7 +341,7 @@ def main():
                                         #wav_file = open('audio.wav', 'wb')
                                         #wav_file.write(audio)
                                 with st.container():
-                                    pred_emotion = get_pred_recorded(audio, 45000)
+                                    pred_emotion = get_pred_recorded(audio.astype(np.float32), 45000)
 
                                 #audio_bytes = audio_recorder(text = 'Click to Record', 
                                                              #recording_color = '2cd2e8', 
