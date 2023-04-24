08b47f60aeafe1eaa9f20627904eea7beee5e82a
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-24 17:26:01 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -339,10 +339,10 @@ def main():
                                     fig2 = plt.figure(figsize = (20, 8))
                                     spectrogram(audio.astype(np.float32), 45000)
                                     st.write(fig2)
-                            with st.container():
-                                pred_emotion = get_pred_recorded(audio.astype(np.float32), 45000)
-                                unique, counts = np.unique(pred_emotion, return_counts = True)
-                                st.markdown('## Emotion Detected: {}'.format(unique[counts.argmax()]))
+                                with st.container():
+                                    pred_emotion = get_pred_recorded(audio.astype(np.float32), 45000)
+                                    unique, counts = np.unique(pred_emotion, return_counts = True)
+                                    st.markdown('## Emotion Detected: {}'.format(unique[counts.argmax()]))
                                 
 
                                 #audio_bytes = audio_recorder(text = 'Click to Record', 
