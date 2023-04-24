0c2cac1d7c27b150147f8389880ceb307e83e1e6
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-23 22:56:59 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from io import BytesIO
 from pydub import AudioSegment, effects
-from ffprobe import FFProbe
+import ffmeg
 
 
 emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Surprise', 'Sad']
