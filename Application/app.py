4f2b24401c413fdc15b939cd5db57de788a524fc
sehanks <116026529+sehanks@users.noreply.github.com>
2023-04-24 20:43:42 -0400

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -296,48 +296,48 @@ def main():
                             if st.button('Record an audio file'):
                                 with column4:
                                     audio = audiorecorder('Click to record', 'Recording...')
-                                with column2:
-                                    st.markdown('#  ')
-                                    st.markdown('#  ')
-                                    if len(audio) > 0:
-                                        st.audio(audio.tobytes()) 
-                                if len(audio) > 0:
-                                    with column1:
-                                        st.markdown('###  ')
-                                        st.markdown('###### Waveplot for Recorded Audio File')
-                                        fig = plt.figure(figsize = (20, 8))
-                                        librosa.display.waveplot(audio.astype(np.float32), sr = 45000)
-                                        plt.gca().axes.get_yaxis().set_visible(False)
-                                        plt.gca().axes.get_xaxis().set_visible(False)
-                                        plt.gca().axes.spines['right'].set_visible(False)
-                                        plt.gca().axes.spines['left'].set_visible(False)
-                                        plt.gca().axes.spines['top'].set_visible(False)
-                                        plt.gca().axes.spines['bottom'].set_visible(False)
-                                        st.write(fig)
                                     with column2:
                                         st.markdown('#  ')
-                                        st.markdown('####  ')
-                                        st.markdown('######   ')
-                                        st.markdown('###### Mel-Spectrogram for Recorded Audio File')
-                                        fig2 = plt.figure(figsize = (20, 8))
-                                        spectrogram(audio.astype(np.float32), 45000)
-                                        st.write(fig2)
-                                    with column1: 
-                                        pred_emotion = get_pred_recorded(audio.astype(np.float32), 45000)
-                                        unique, counts = np.unique(pred_emotion, return_counts = True)
-                                        st.markdown('## Emotion Detected: {}'.format(unique[counts.argmax()]))
-                                    with column2: 
-                                        fig = plt.figure(figsize = (15, 7))
-                                        unique, counts = np.unique(pred_emotion, return_counts = True)
-                                        bar = np.asarray((unique, counts)).T
-                                        col = list(bar[:, 0])
-                                        row = list(bar[:, 1])
-                                        color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-                                        plt.bar(x = col, height = [int(x) for x in row], color = color)
-                                        plt.xlabel('Emotions Detected')
-                                        plt.ylabel('Frequency')
-                                        plt.title('Frequency of Emotions Detected')
-                                        st.pyplot(fig)
+                                        st.markdown('#  ')
+                                        if len(audio) > 0:
+                                            st.audio(audio.tobytes()) 
+                                    if len(audio) > 0:
+                                        with column1:
+                                            st.markdown('###  ')
+                                            st.markdown('###### Waveplot for Recorded Audio File')
+                                            fig = plt.figure(figsize = (20, 8))
+                                            librosa.display.waveplot(audio.astype(np.float32), sr = 45000)
+                                            plt.gca().axes.get_yaxis().set_visible(False)
+                                            plt.gca().axes.get_xaxis().set_visible(False)
+                                            plt.gca().axes.spines['right'].set_visible(False)
+                                            plt.gca().axes.spines['left'].set_visible(False)
+                                            plt.gca().axes.spines['top'].set_visible(False)
+                                            plt.gca().axes.spines['bottom'].set_visible(False)
+                                            st.write(fig)
+                                        with column2:
+                                            st.markdown('#  ')
+                                            st.markdown('####  ')
+                                            st.markdown('######   ')
+                                            st.markdown('###### Mel-Spectrogram for Recorded Audio File')
+                                            fig2 = plt.figure(figsize = (20, 8))
+                                            spectrogram(audio.astype(np.float32), 45000)
+                                            st.write(fig2)
+                                        with column1: 
+                                            pred_emotion = get_pred_recorded(audio.astype(np.float32), 45000)
+                                            unique, counts = np.unique(pred_emotion, return_counts = True)
+                                            st.markdown('## Emotion Detected: {}'.format(unique[counts.argmax()]))
+                                        with column2: 
+                                            fig = plt.figure(figsize = (15, 7))
+                                            unique, counts = np.unique(pred_emotion, return_counts = True)
+                                            bar = np.asarray((unique, counts)).T
+                                            col = list(bar[:, 0])
+                                            row = list(bar[:, 1])
+                                            color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+                                            plt.bar(x = col, height = [int(x) for x in row], color = color)
+                                            plt.xlabel('Emotions Detected')
+                                            plt.ylabel('Frequency')
+                                            plt.title('Frequency of Emotions Detected')
+                                            st.pyplot(fig)
                                 
     
         if audio_file is not None:
